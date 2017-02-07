80593b12411156bef2e91274ea7efd29088b8499
Thiago de Araujo Silva <TasThief@gmail.com>
2017-02-06 21:28:54 -0800

    Fixed threadpool
    still need to test it better when i have more tasks for the thread pool

--- Elucidara 4.0/ThreadPool.cpp
+++ Elucidara 4.0/ThreadPool.cpp
@@ -10,6 +10,7 @@ manually call start_cycle to put the current thread into a infinite cycle of del
 care when calling start_cycle through the main thread.
 Author: Thiago A. Silva
 */
+//#define threadDebug
 
 ThreadPool* ThreadPool::s = nullptr;
 
@@ -20,60 +21,126 @@ void ThreadPool::add_command(function<void()>  command) {
 	s->ready.notify_one();
 }
 
-//kill all the threads in the pool
-void ThreadPool::kill() {
-	s->run = false;
-}
-
 //set the thread to solve commands
-void ThreadPool::start_cycle()
+void ThreadPool::start_cycle(ThreadInfo* info)
 {
-	//inialize a method pointer to load the stashed methods
-	function<void()> function = nullptr;
+	unique_lock<mutex> lock(s->queuelock);
 
-	//start thread cycle
-	while (s->run)
+	while (info->isRunning)
 	{
+
+		if (s->commandQueue.empty())
 		{
-			//this lock ensures the thread safety to command queue 
-			unique_lock<mutex> lock(s->queuelock);
 
-			//if the queue is empty
-			if (s->commandQueue.empty())
+#if defined threadDebug
+			cout << "hibernating" << endl;
+#endif
 
-				//sleep til a command is stashed
-				s->ready.wait(lock, []() {return !s->commandQueue.empty();});
+			info->isAwake = false;
+
+			s->ready.wait(lock, []() {return !s->commandQueue.empty();});
+
+			info->isAwake = true;
+
+#if defined threadDebug
+			cout << "waking" << endl;
+#endif
+
 		}
+#if defined threadDebug
+		else
 		{
-			//this lock (again) ensures the thread safety to command queue 
-			lock_guard<mutex> lock(s->queuelock);
-
-			//save the top command
-			function = s->commandQueue.front();
-
-			//remove it from the top of the queue
-			s->commandQueue.pop();
+			cout << "no need for sleep" << endl;
 		}
-		//execute it
-		function();
+#endif
 
-		//delete it
-		function = nullptr;
+		info->process = s->commandQueue.front();
+
+		s->commandQueue.pop();
+		
+		lock.unlock();
+
+		info->process();
+
+#if defined threadDebug
+		cout << "process finished"<<endl;
+#endif
+
+		lock.lock();
+
+		info->process = nullptr;
 	}
+	info->isAlive = false;
+
+	info->aliveBlock.notify_all();
 }
 
 //initialize x ammount of threads and put them into the infinite resolution cycle 
-ThreadPool::ThreadPool(const int threadcount)
+ThreadPool::ThreadPool(int threadcount)
 {
 	s = this;
+	
 	for (size_t i = 0; i < threadcount; i++) {
-		thread daemon(ThreadPool::start_cycle);
+		
+		threadInfos.push_back(new ThreadInfo());
+		threadInfos[i]->id = i;
+
+		thread daemon(ThreadPool::start_cycle,threadInfos[i]);
 		daemon.detach();
 	}
 }
-
+ThreadPool::ThreadPool() {}
 //kill all threads
 ThreadPool::~ThreadPool()
 {
-	kill();
+	for (vector<ThreadInfo*>::iterator itr = threadInfos.begin(); itr != threadInfos.end(); itr++){
+		unique_lock<mutex> lock((*itr)->aliveMutex);
+		if ((*itr)->isAlive)
+			(*itr)->aliveBlock.wait(lock, [itr]() { return !(*itr)->isAlive;});
+		delete *itr;
+	}
 }
+
+
+
+/*
+//set the thread to solve commands
+void ThreadPool::start_cycle(ThreadInfo info)
+{
+//start thread cycle
+while (info.isRunning)
+{
+{
+//this lock ensures the thread safety to command queue
+unique_lock<mutex> lock(s->queuelock);
+
+//if the queue is empty
+if (s->commandQueue.empty())
+{
+info.isAwake = false;
+//sleep til a command is stashed
+s->ready.wait(lock, []() {return !s->commandQueue.empty();});
+
+info.isAwake = true;
+}
+}
+{
+//this lock (again) ensures the thread safety to command queue
+lock_guard<mutex> lock(s->queuelock);
+
+//save the top command
+info.process = s->commandQueue.front();
+
+//remove it from the top of the queue
+s->commandQueue.pop();
+}
+//execute it
+info.process();
+
+//delete it
+info.process = nullptr;
+}
+info.isAlive = false;
+}
+
+*/
--- Elucidara 4.0/ThreadPool.h
+++ Elucidara 4.0/ThreadPool.h
@@ -1,34 +1,43 @@
 #pragma once
 
+struct ThreadInfo {
+	int id = 0;
+	bool isRunning = true;
+	bool isAlive = true;
+	bool isAwake = true;
+	
+	function<void()> process = nullptr;
+	condition_variable aliveBlock;
+	mutex aliveMutex;
+
+	ThreadInfo(){}
+};
+
 class ThreadPool
 {
 private:
-	//singleton instance
 	static ThreadPool* s;
 
-	//mutex for the command queue
 	mutex queuelock;
 
-	//switch to kill threads
-	bool run = true;
+	mutex coutmutex;
 
-	//flux controll condition
 	condition_variable ready;
 
-	//command queue
 	queue<function<void()> > commandQueue;
+
+	vector<ThreadInfo*> threadInfos;
+
 
 public:
 
-	//adds a command to be processed
 	static void add_command(function<void()>  command);
 
-	static void kill();
+	static void start_cycle(ThreadInfo* info);
 
-	static void start_cycle();
-
-	ThreadPool(const int threadcount);
-
+	ThreadPool(int threadcount);
+	ThreadPool();
 	~ThreadPool();
+
 };
 
